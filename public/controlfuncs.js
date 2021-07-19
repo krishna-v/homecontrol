86f65ef070bd74abf910515825159b34eacb4bb1
krishna-v <krishna.visweswaran@gmail.com>
2021-07-19 18:31:39 +0530

    code cleanup

--- public/controlfuncs.js
+++ public/controlfuncs.js
@@ -1,36 +1,34 @@
-window.onload = initApp;
+window.onload = loadURL("/model.json", loadUI);
 
 const popupmap = {
 	"smartlight": openSmartLightPopup
 };
 
-function initApp() {
-	var xhttp = new XMLHttpRequest();
+function loadURL(url, callback) {
+	const xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if(this.readyState == 4 && this.status == 200) {
-			loadUI(JSON.parse(this.responseText));
+			callback(JSON.parse(this.responseText));
 		}
 	};
-	var url = "/model.json";
 	xhttp.open("GET", url, true);
 	xhttp.send();
-	// buildSmartLightPopup();
 }
 
 function loadUI(model) {
-	var wrapper = document.getElementById("appwrapper");
-	var homebox = document.getElementById("home");
-	var logo = document.getElementById("logo");
-	var menubox = document.getElementById("menubox");
+	const wrapper = document.getElementById("appwrapper");
+	const homebox = document.getElementById("home");
+	const logo = document.getElementById("logo");
+	const menubox = document.getElementById("menubox");
 	logo.onclick = function() { showUI("home"); };
 	menubox.onclick = function() { showUI("activectrls"); };
 
-	for(var room_id in model.rooms) {
+	for(let room_id in model.rooms) {
 		console.log("Building room " + room_id);
-		var roombox = document.createElement("div");
+		const roombox = document.createElement("div");
 		roombox.id = "roombox_" + room_id;
 		roombox.setAttribute("class", "roombox");
-		var roombtn = document.createElement("button");
+		const roombtn = document.createElement("button");
 		roombtn.id = "roomlink_" + room_id;
 		roombtn.setAttribute("class", "roomlink");
 		roombtn.setAttribute("room_id", "room_" + room_id);
@@ -38,33 +36,33 @@ function loadUI(model) {
 		roombtn.innerHTML = model.rooms[room_id].name;
 		roombox.appendChild(roombtn);
 		homebox.appendChild(roombox);
-		var room = createroom(room_id, model.rooms[room_id]);
-		appwrapper.appendChild(room);
+		const room = createroom(room_id, model.rooms[room_id]);
+		wrapper.appendChild(room);
 	}
-	updateStateNew();
+	loadURL("/dynamic/fullmap.json", refreshState);
 	showUI("home");
 }
 
 function createroom(room_id, rinfo) {
-	var active = document.getElementById("activectrls");
-	var room = document.createElement("div");
+	const active = document.getElementById("activectrls");
+	const room = document.createElement("div");
 	room.id = "room_" + room_id;
 	room.setAttribute("class", "container room");
 	room.setAttribute("name", rinfo.name);
 	room.setAttribute("num", room_id);
 	room.setAttribute("state", "hidden");
-	for(var ctrl_id in rinfo.ctrls) {
-		var ctrl = rinfo.ctrls[ctrl_id];
-		var control = document.createElement("div");
+	for(let ctrl_id in rinfo.ctrls) {
+		const ctrl = rinfo.ctrls[ctrl_id];
+		const control = document.createElement("div");
 		control.id = "ctrl_div_" + room_id + "_" + ctrl_id;
 		control.setAttribute("class", "control");
 		control.setAttribute("type", ctrl.type);
 
-		var dup_div = control.cloneNode(false);
+		const dup_div = control.cloneNode(false);
 		dup_div.id = "act_dup_div_" + room_id + "_" + ctrl_id;
 		dup_div.setAttribute("childstate", "off");
 
-		var btn = document.createElement("button");
+		const btn = document.createElement("button");
 		btn.id = "ctrl_" + room_id + "_" + ctrl_id;
 		btn.setAttribute("name", btn.id);
 		btn.setAttribute("class", ctrl.type.toLowerCase());
@@ -72,9 +70,9 @@ function createroom(room_id, rinfo) {
 		btn.onclick = function() { flip_state(this); };
 		btn.innerHTML = ctrl.name;
 
-		var dup_ctrl = btn.cloneNode(false);
+		const dup_ctrl = btn.cloneNode(false);
 		dup_ctrl.id = "act_dup_" + btn.id;
-		var clsname = btn.getAttribute("class") + " dup_" + btn.id;
+		const clsname = btn.getAttribute("class") + " dup_" + btn.id;
 		dup_ctrl.setAttribute("class", clsname);
 		dup_ctrl.onclick = function() { flip_state(this); };
 		btn.oncontextmenu = function() { openPopup(this); };
@@ -91,53 +89,29 @@ function createroom(room_id, rinfo) {
 
 function showUI(name) {
 	console.log("showUI : " + name);
-	var list = document.getElementsByClassName("container");
-	for(var i = 0; i < list.length; i++) {
+	const list = document.getElementsByClassName("container");
+	for(let i = 0; i < list.length; i++) {
 		list[i].setAttribute("state", "hidden");
 	}
-	var active = document.getElementById(name);
+	const active = document.getElementById(name);
 	active.setAttribute("state", "active");
 	document.getElementById("pagetitle").innerHTML = active.getAttribute("name");
 }
 
-var reload = setInterval(updateStateNew, 10000);
-
-function updatestate() {
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-		if(this.readyState == 4 && this.status == 200) {
-			refreshstate(JSON.parse(this.responseText));
-		}
-	};
-	var url = "/dynamic/statemap.json";
-	xhttp.open("GET", url, true);
-	xhttp.send();
-}
-
-function updateStateNew() {
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-		if(this.readyState == 4 && this.status == 200) {
-			refreshStateNew(JSON.parse(this.responseText));
-		}
-	};
-	var url = "/dynamic/fullmap.json";
-	xhttp.open("GET", url, true);
-	xhttp.send();
-}
+var reload = setInterval(loadURL, 10000, "/dynamic/fullmap.json", refreshState);
 
 function set_state(ctrl, value) {
-	var name = ctrl.getAttribute("name");
-	var realctrl = document.getElementById(name);
-	var query="/dynamic/ctrl/" + name + "?value=" + value;
+	const name = ctrl.getAttribute("name");
+	const realctrl = document.getElementById(name);
+	const query="/dynamic/ctrl/" + name + "?value=" + value;
 	console.log("flip_state: query = " + query);
-	var xhttp = new XMLHttpRequest();
+	const xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if(this.readyState == 4) {
 			if(this.status == 200 || this.status == 204) {
 				realctrl.setAttribute("pending", "true");
 				realctrl.setAttribute("pendcount", 0);
-				var state = value == 1 ? "on" : "off";
+				const state = value == 1 ? "on" : "off";
 				realctrl.setAttribute("state", state);
 				update_dups(realctrl);
 			}
@@ -151,14 +125,14 @@ function set_state(ctrl, value) {
 }
 
 function set_color(ctrl, color) {
-	var name = ctrl.getAttribute("name");
-	var realctrl = document.getElementById(name);
-	var query="/dynamic/ctrl/" + name + "?color=" + color;
-	var xhttp = new XMLHttpRequest();
+	const name = ctrl.getAttribute("name");
+	const realctrl = document.getElementById(name);
+	const query="/dynamic/ctrl/" + name + "?color=" + color;
+	const xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if(this.readyState == 4) {
 			if(this.status == 200 || this.status == 204) {
-				let state = "on";
+				const state = "on";
 				realctrl.setAttribute("pending", "true");
 				realctrl.setAttribute("pendcount", 0);
 				realctrl.setAttribute("state", state);
@@ -174,71 +148,29 @@ function set_color(ctrl, color) {
 }
 
 function flip_state(ctrl) {
-	var newval = ctrl.getAttribute('state') == "on" ? 0 : 1;
+	const newval = ctrl.getAttribute('state') == "on" ? 0 : 1;
 	set_state(ctrl, newval);
 }
 
-function refreshstate(data) {
-	console.log("Refresh State (old)...");
-	for(var key in data) {
-		var arr = key.split(".");
-		var room_id = arr[0];
-		var ctrl_id = arr[1];
-		var value = data[key];;
-		var state;
-		var color = null;
-		if(typeof value == "string") {
-			state = value;
-		} else {
-			state = value.state;
-			color = value.color;
-		}
-		var node = document.getElementById("ctrl_" + room_id + "_" + ctrl_id);
-		if(node) {
-			var nodestate = node.getAttribute("state");
-			if(color != null) {
-				node.setAttribute("lightcolor", color);
-			}
-			if(state != nodestate) {
-				var pending = node.getAttribute("pending");
-				var pendcount = node.getAttribute("pendcount");
-				if(pending == "true") {
-					console.log(node.id + ": state mismatch. pc: " + pendcount);
-					if(pendcount < 2) {
-						node.setAttribute("pendcount", parseInt(pendcount) + 1);
-						continue;
-					}
-				}
-				node.setAttribute("state", state);
-				update_dups(node);
-			}
-			node.setAttribute("pending", "false");
-			node.setAttribute("pendcount", "0");
-		} else {
-			console.log("Can't find node. room_id: " + room_id + ", control ID: " + ctrl_id);
-		}
-	}
-}
-
-function refreshStateNew(data) {
+function refreshState(data) {
 	let now = Date.now();
-	for(var room_id in data.rooms) {
-		for(var ctrl_id in data.rooms[room_id].ctrls) {
-			var state = data.rooms[room_id].ctrls[ctrl_id].state;
+	for(let room_id in data.rooms) {
+		for(let ctrl_id in data.rooms[room_id].ctrls) {
+			const state = data.rooms[room_id].ctrls[ctrl_id].state;
 			if(state === undefined || state == null) continue;
-			var color = data.rooms[room_id].ctrls[ctrl_id].color;
-			var lastchanged = data.rooms[room_id].ctrls[ctrl_id].lastChanged;
-			var node = document.getElementById("ctrl_" + room_id + "_" + ctrl_id);
+			const color = data.rooms[room_id].ctrls[ctrl_id].color;
+			let lastchanged = data.rooms[room_id].ctrls[ctrl_id].lastChanged;
+			const node = document.getElementById("ctrl_" + room_id + "_" + ctrl_id);
 			if(node) {
 				let needs_update = false;
-				var nodestate = node.getAttribute("state");
-				if(color != null) {
+				const nodestate = node.getAttribute("state");
+				if(color !== null) {
 					node.setAttribute("lightcolor", color);
 				}
-				if(state != nodestate) {
-					var pending = node.getAttribute("pending");
-					var pendcount = node.getAttribute("pendcount");
-					if(pending == "true") {
+				if(state !== nodestate) {
+					const pending = node.getAttribute("pending");
+					const pendcount = node.getAttribute("pendcount");
+					if(pending === "true") {
 						console.log(node.id + ": state mismatch. pc: " + pendcount);
 						if(pendcount < 2) {
 							node.setAttribute("pendcount", parseInt(pendcount) + 1);
@@ -251,7 +183,7 @@ function refreshStateNew(data) {
 				}
 				node.setAttribute("pending", "false");
 				node.setAttribute("pendcount", "0");
-				if(lastchanged !== undefined && lastchanged != null) {
+				if(lastchanged !== undefined && lastchanged !== null) {
 					node.setAttribute("lastchanged", lastchanged);
 					lastchanged = Number(lastchanged);
 					if(state == "on" && (now - lastchanged) > 7200000 &&
@@ -261,7 +193,6 @@ function refreshStateNew(data) {
 						needs_update = true;
 					}
 				}
-
 				if(needs_update) update_dups(node);
 			} else {
 				console.log("Can't find node. room_id: " + room_id + ", control ID: " + ctrl_id);
@@ -271,19 +202,19 @@ function refreshStateNew(data) {
 }
 
 function update_dups(node) {
-	var state = node.getAttribute("state");
-	var color = node.getAttribute("lightcolor");
-	var lastchanged = node.getAttribute("lastchanged");
-	var ontime = node.getAttribute("ontime");
+	const state = node.getAttribute("state");
+	const color = node.getAttribute("lightcolor");
+	const lastchanged = node.getAttribute("lastchanged");
+	const ontime = node.getAttribute("ontime");
 
 	// console.log("updating dups..." + node.getAttribute("name"));
-	var dupname = "dup_" + node.id;
-	var duplist = document.getElementsByClassName(dupname);
-	for(var i = 0; i < duplist.length; i++) {
+	const dupname = "dup_" + node.id;
+	const duplist = document.getElementsByClassName(dupname);
+	for(let i = 0; i < duplist.length; i++) {
 		duplist[i].setAttribute("state", state);
 		if(color !== undefined && color != null) duplist[i].setAttribute("lightcolor", color);
-		if(lastchanged !== undefined && lastchanged != null) duplist[i].setAttribute("lastchanged", lastchanged);
-		if(ontime !== undefined && ontime != null) duplist[i].setAttribute("ontime", ontime);
+		if(lastchanged !== undefined && lastchanged !== null) duplist[i].setAttribute("lastchanged", lastchanged);
+		if(ontime !== undefined && ontime !== null) duplist[i].setAttribute("ontime", ontime);
 		duplist[i].parentNode.setAttribute("childstate", state == "offline" ? "off" : state);
 	}
 }
@@ -291,42 +222,42 @@ function update_dups(node) {
 /* ----------------- Popup Stuff -------------------------------*/
 
 function buildSmartlightTopBox() {
-	var topbox = document.createElement("div");
-	topbox.id = name + "top-ctrls";	
+	const topbox = document.createElement("div");
+	topbox.id = "top-ctrls";	
 	topbox.setAttribute("class", "smartlight-topbox");
 
-	var lightmode = document.createElement("div");
+	const lightmode = document.createElement("div");
 	lightmode.id = "lightmode";
 	lightmode.setAttribute("class", "popup-label");
 	lightmode.innerHTML = "Light Mode:";
 	topbox.appendChild(lightmode);
 
-	var switchwrap = document.createElement("div");
+	const switchwrap = document.createElement("div");
 	switchwrap.id = "switchwrap";
 	switchwrap.setAttribute("class", "flexwrap");
 
-	var switchlabel = document.createElement("label");
+	const switchlabel = document.createElement("label");
 	switchlabel.id = "top-switchlabel";
 	switchlabel.setAttribute("class", "switch");
 
-	var modetoggle = document.createElement("input");
+	const modetoggle = document.createElement("input");
 	modetoggle.id = "modeToggle";
 	modetoggle.setAttribute("type", "checkbox");
 	modetoggle.onclick = function() { togglemode(this); };
 	switchlabel.appendChild(modetoggle);
 
-	var opmodediv = document.createElement("div");
+	const opmodediv = document.createElement("div");
 	opmodediv.id = "opmode";
 	opmodediv.setAttribute("class", "toggle opmode");
 	switchlabel.appendChild(opmodediv);
 	switchwrap.appendChild(switchlabel);
 	topbox.appendChild(switchwrap);
 
-	var applywrap = document.createElement("div");
+	const applywrap = document.createElement("div");
 	applywrap.id = "applywrap";
 	applywrap.setAttribute("class", "flexwrap");
 
-	var applybutton = document.createElement("button");
+	const applybutton = document.createElement("button");
 	applybutton.id = "apply";
 	applybutton.onclick = function() { execPopup(); };
 	applybutton.innerHTML = "Apply!";
@@ -337,28 +268,28 @@ function buildSmartlightTopBox() {
 }
 
 function sliderChanged(slider) {
-	let val = slider.parentElement.parentElement.querySelector(".slider-value");
+	const val = slider.parentElement.parentElement.querySelector(".slider-value");
 	if(val !== undefined && val !== null) val.innerHTML = slider.value;
 	else console.log("Can't find the value box for " + slider.id);
 }
 
 function buildSlider(name, label, min, max, start) {
-	var sliderbox = document.createElement("div");
+	const sliderbox = document.createElement("div");
 	sliderbox.id = name + "-box";	
 	sliderbox.setAttribute("class", "sliderbox");
 	sliderbox.setAttribute("state", "hidden");
 
-	var labeldiv = document.createElement("div");
+	const labeldiv = document.createElement("div");
 	labeldiv.id = name + "-label";
 	labeldiv.setAttribute("class", "popup-label");
 	labeldiv.innerHTML = label;
 	sliderbox.appendChild(labeldiv);
 
-	var rangediv = document.createElement("div");
+	const rangediv = document.createElement("div");
 	rangediv.id = name + "-slider";
 	rangediv.setAttribute("class", "slider-wrap");
 
-	var range = document.createElement("input");
+	const range = document.createElement("input");
 	range.id = name + "-range";
 	range.setAttribute("type", "range");
 	range.setAttribute("class", "range");
@@ -370,7 +301,7 @@ function buildSlider(name, label, min, max, start) {
 	rangediv.appendChild(range);
 	sliderbox.appendChild(rangediv);
 
-	var valuebox = document.createElement("div");
+	const valuebox = document.createElement("div");
 	valuebox.id = name + "-value";
 	valuebox.setAttribute("class", "slider-value");
 	valuebox.innerHTML = start;
@@ -380,16 +311,16 @@ function buildSlider(name, label, min, max, start) {
 }
 
 function buildInfoBox(ctrl) {
-	var infobox = document.createElement("div");
+	const infobox = document.createElement("div");
 	infobox.id = "popup-infobox";
 	infobox.setAttribute("class", "popup-infobox");
-	let changets = ctrl.getAttribute("lastchanged");
+	const changets = ctrl.getAttribute("lastchanged");
 	let lastchanged = "unknown";
 	if(changets !== undefined || changets != null) {
 		console.log("Lastchanged: " + changets);
 		try {
 			lastchanged = new Date(Number(changets)).toLocaleString('sv', { timeZone: 'Asia/Kolkata' });
-			if(lastchanged.toString() == "Invalid Date") throw new RangeError("Invalid Date");
+			if(lastchanged.toString() === "Invalid Date") throw new RangeError("Invalid Date");
 		} catch(e) {
 			lastchanged = "{ " + new Date(Number(changets)).toLocaleString( 'sv', { timeZoneName: 'short' } ).replace(/ GMT.*/, "") + " }";
 		}
@@ -399,7 +330,7 @@ function buildInfoBox(ctrl) {
 }
 
 function buildSmartLightPopup(ctrl) {
-	var content = document.getElementById("popupcontent");
+	const content = document.getElementById("popupcontent");
 	content.appendChild(buildSmartlightTopBox());
 	content.appendChild(buildSlider("ctemp", "White/Yellow :", 0, 100, 100));
 	content.appendChild(buildSlider("lum", "Brightness :", 0, 255, 128));
@@ -410,19 +341,19 @@ function buildSmartLightPopup(ctrl) {
 }
 
 function openPopup(ctrl) {
-	var name = ctrl.getAttribute("name");
-	var realctrl = document.getElementById(name);
+	const name = ctrl.getAttribute("name");
+	const realctrl = document.getElementById(name);
 	
-	let ctrltype = realctrl.getAttribute("class");
+	const ctrltype = realctrl.getAttribute("class");
 	console.log("Getting popup func for type " + ctrltype);
 	let popupFunc = popupmap[realctrl.getAttribute("class")];
 	if(popupFunc === undefined || popupFunc == null) popupFunc = openInfoPopup;
 	document.getElementById("popup-title").innerHTML = realctrl.innerHTML;
-	var content = document.getElementById("popupcontent");
+	const content = document.getElementById("popupcontent");
 	while(content.firstChild) { content.removeChild(content.firstChild); }
 	popupFunc(realctrl);
 
-	var popup = document.getElementById("ctrl-popup");
+	const popup = document.getElementById("ctrl-popup");
 	popup.setAttribute("ctrl_id", ctrl.id);
 	popup.setAttribute("state", "active");
 	event.preventDefault();
@@ -431,7 +362,7 @@ function openPopup(ctrl) {
 
 function openInfoPopup(ctrl) {
 	console.log("openInfoPopup called");
-	var content = document.getElementById("popupcontent");
+	const content = document.getElementById("popupcontent");
 	content.appendChild(buildInfoBox(ctrl));
 }
 
@@ -493,10 +424,10 @@ function togglemode(cb) {
 };
 
 function execPopup() {
-	var r = 0, g = 0, b = 0, cw = 0, ww = 0, hexcolor="";
+	let r = 0, g = 0, b = 0, cw = 0, ww = 0, hexcolor="";
 	if(document.getElementById("modeToggle").checked) {
-		var temp = document.getElementById("ctemp-range").value;
-		var lum = document.getElementById("lum-range").value;
+		const temp = document.getElementById("ctemp-range").value;
+		const lum = document.getElementById("lum-range").value;
 		cw = Math.round(((100 - temp) / 100) * lum);
 		ww = Math.round((temp / 100) * lum);
 	} else {
@@ -511,7 +442,7 @@ function execPopup() {
 		(+ww).toString(16).padStart(2, '0');
 
 	console.log("Setting smart bulb color to " + hexcolor);
-	var ctrl_id = document.getElementById("ctrl-popup").getAttribute("ctrl_id");
-	var ctrl = document.getElementById(ctrl_id);
+	const ctrl_id = document.getElementById("ctrl-popup").getAttribute("ctrl_id");
+	const ctrl = document.getElementById(ctrl_id);
 	set_color(ctrl, hexcolor);
 }
